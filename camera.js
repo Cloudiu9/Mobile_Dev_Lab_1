4c6ddb459e7a56d409d213de72102029ae80443e
Bordea Claudiu <123594015+Cloudiu9@users.noreply.github.com>
2023-10-24 12:03:29 +0300

    First commit

--- camera.js
+++ camera.js
@@ -1,7 +1,7 @@
 var video = document.getElementById("video");
 
 video.addEventListener("touchstart", capture);
- video.addEventListener("mousedown", capture);
+video.addEventListener("mousedown", capture);
 
 function on_cam_success(stream)
 {
@@ -13,7 +13,7 @@ function on_cam_error(err)
     alert("Error. "+err.message);
 }
 
-var constraints = { audio:false, video:true };
+var constraints = { audio: false, video: true };
 navigator.mediaDevices.getUserMedia(constraints)
 .then(on_cam_success)
 .catch(on_cam_error);
@@ -22,7 +22,7 @@ function capture()
 {
     var c = document.getElementById("canvas");
     c.width = video.width;
-    c.height = video.height
+    c.height = video.height;
     var ctx = c.getContext("2d");
     ctx.drawImage(video, 0, 0, 640, 480);
 }
